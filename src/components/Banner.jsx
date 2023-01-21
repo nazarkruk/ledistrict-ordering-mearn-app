facc5b032cbf5c931a205a328a4d2bf25349929a
Your NameNazar <nazarkruk@icloud.com>
2023-01-20 22:57:45 -0500

    Structuring Navigation

---
+++ src/components/Banner.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+function Banner() {
+  return (
+    <div className="banner w-full md:w-2/3 px-7 mx-auto relative flex items-center-justify-between">
+      <div className="banner-description w-full md:w-1/2 p-3"></div>
+      <div className="banner-image w-full md:w-2/3 p-3"></div>
+    </div>
+  );
+}
+
+export default Banner;
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,18 +1,34 @@
+import ld_logo from "../assets/images/ld_logo.png";
+import cartIcon from "../assets/icons/cart.svg";
+
+import { Link } from "react-router-dom";
+
 export const Header = () => {
   return (
-    <nav id="header" className="bg-blak text-white">
-      <div className="container w-full mx-auto flex flex-wrap items-center justify-content-between mt-0 py-2">
+    <nav id="header" className="bg-black text-white">
+      <div className="container w-full mx-auto flex flex-wrap items-center justify-between mt-0 py-2">
         <div className="logo-wrapper pl-4 flex items-center">
-          <img src={"/"} alt="logo" />
+          <Link
+            to="/"
+            className="toggleColor text-white no-underline hover:no-underline font-bold text-2xl lg:text-4xl"
+          >
+            <img src={ld_logo} alt="logo" className="w-40 my-5 object-cover" />
+          </Link>
         </div>
         <div className="nav-menu-wrapper flex items-center justify-between space-x-10">
-          <div>Home</div>
-          <div>About</div>
+          <Link to="/" className="text-xl">
+            Home
+          </Link>
+          <Link to="#about" className="text-xl">
+            About
+          </Link>
         </div>
         <div className="flex itms-center justify-content-center space-x-4">
-          <div>Cart</div>
-          <div>Log In</div>
-          <dive>Sign Up</dive>
+          <Link to="/cart">
+            <img src={cartIcon} alt="cart" />
+          </Link>
+          <Link to="/login">Log In</Link>
+          <Link to="/register">Sign Up</Link>
         </div>
       </div>
     </nav>
