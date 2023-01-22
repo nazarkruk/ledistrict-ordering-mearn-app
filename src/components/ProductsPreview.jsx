f7bd318969419cd0fd14909fcfd80547fcb26c14
Your NameNazar <nazarkruk@icloud.com>
2023-01-22 01:12:15 -0500

    Connecting db

---
+++ src/components/ProductsPreview.jsx
@@ -0,0 +1,9 @@
+import React, { useState, useEffect } from "react";
+
+export const ProductsPreview = () => {
+  return (
+    <div className="container mx-auto pb-4 w-2/3 text-white bg-black">
+      <h2>Products</h2>
+    </div>
+  );
+};
