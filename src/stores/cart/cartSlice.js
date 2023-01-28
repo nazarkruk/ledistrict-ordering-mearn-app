6084d859e2639dd4ddecc8dce850b66dd36ed9b6
Your NameNazar <nazarkruk@icloud.com>
2023-01-27 19:49:47 -0500

    Building car Slice

---
+++ src/stores/cart/cartSlice.js
@@ -0,0 +1,38 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+const initioalState = {
+  products: [],
+};
+
+export const createSlice = creatSlice({
+  name: "cart",
+  initioalState,
+  reducers: {
+    addToCard: (state, action) => {
+      return {
+        products: [...state.products, { ...action.payload, amount: 1 }],
+      };
+    },
+    clearCart: (state) => {
+      return { products: [] };
+    },
+    incrementProductAmount: (state, action) => {
+      return {
+        products: state.products.map((product) =>
+          product.id === action.payload.id
+            ? { ...product, amount: product.amount + 1 }
+            : product
+        ),
+      };
+    },
+    decrementProductAmount: (state, action) => {
+      return {
+        products: state.products.map((product) =>
+          product.id === action.payload.id
+            ? { ...product, amount: product.amount - 1 }
+            : product
+        ),
+      };
+    },
+  },
+});
