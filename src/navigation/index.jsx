09fd78c37808c2fe6e53a17894f7271a2a88003f
Your NameNazar <nazarkruk@icloud.com>
2023-02-08 22:46:50 -0500

    Addinng Footer and fixing Menu Tabs bug

--- src/navigation/index.jsx
+++ src/navigation/index.jsx
@@ -2,15 +2,20 @@ import { BrowserRouter, Route, Routes } from "react-router-dom";
 import { Header } from "../components/Header";
 import Home from "../pages/Home";
 import Login from "../pages/Login";
-import Menu from "../pages/Menu";
-import PaymentSuccess from "../pages/PaymentSuccess";
-import Cart from "../pages/Cart";
 import Register from "../pages/Register";
+import Menu from "../pages/Menu";
+import Cart from "../pages/Cart";
+import PaymentSuccess from "../pages/PaymentSuccess";
+import { useSelector } from "react-redux";
+import { cartProducts } from "../stores/cart/cartSlice";
+import { Footer } from "../components/Footer";
 
 const Navigation = () => {
+  const productsInCart = useSelector(cartProducts);
+
   return (
     <BrowserRouter>
-      <Header />
+      <Header cartCount={productsInCart ? productsInCart.length : 0} />
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/login" element={<Login />} />
@@ -19,6 +24,7 @@ const Navigation = () => {
         <Route path="/cart" element={<Cart />} />
         <Route path="/payment-success" element={<PaymentSuccess />} />
       </Routes>
+      <Footer />
     </BrowserRouter>
   );
 };
--- src/stores/rootStore.js
+++ src/stores/rootStore.js
@@ -2,6 +2,6 @@ import { configureStore, applyMiddleware } from "@reduxjs/toolkit";
 import rootReducer from "./rootReducer";
 import thunk from "redux-thunk";
 
-const store = configureStore({ reducer: rootReducer }, applyMiddleware(thunk));
+const store = configureStore({ reducer: rootReducer}, applyMiddleware(thunk));
 
 export default store;
